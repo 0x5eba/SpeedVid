8007d93e9ff2431ee1d47e6f07a1d325ef6312e4
Sebastien Biollo <sbiollo@gmail.com>
2020-03-12 02:07:36 +0100

    try some fix

--- src/content.js
+++ src/content.js
@@ -1,12 +1,12 @@
 var curr_speed = "x1"
-var curr_video = 0
+var curr_video = -1
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if(request.type === "getAllVideos"){
-        let videos = document.querySelectorAll('video')
-        let iframes = document.querySelectorAll('iframe')
+        var videos = document.querySelectorAll('video')
+        var iframes = document.querySelectorAll('iframe')
 
-        let len_videos = videos.length
+        var len_videos = videos.length
 
         function getFrameContents(id_iframe){
             var iFrame = document.getElementById(id_iframe);
@@ -17,19 +17,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 iFrameBody = iFrame.contentWindow.document.getElementsByTagName('body')[0];
             }
 
-            let videos_iframe = iFrameBody.querySelectorAll('video')
-
-            return videos_iframe.length
+            var videos_iframe = iFrameBody.querySelectorAll('video')
+            len_videos += videos_iframe.length
         }
         
         for(let a = 0; a < iframes.length; ++a){
             let id = iframes[a].id
-            len_videos += getFrameContents(id)
+            getFrameContents(id)
         }
 
-        // chrome.runtime.sendMessage({
-        //     videos: videos
-        // });
         sendResponse({"tot_videos": len_videos})
         return true
     }
@@ -56,35 +52,40 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             let id = iframes[a].id
             getFrameContents(id)
         }
-
+        
+        var speed = 1
         if(curr_speed === "x05"){
-            videos[curr_video].playbackRate = 0.5
+            speed = 0.5
         } else if(curr_speed === "x1"){
-            videos[curr_video].playbackRate = 1
+            speede = 1
         } else if(curr_speed === "x15"){
-            videos[curr_video].playbackRate = 1.5
+            speed = 1.5
         } else if(curr_speed === "x2"){
-            videos[curr_video].playbackRate = 2
+            speed = 2
         } else if(curr_speed === "x3"){
-            videos[curr_video].playbackRate = 3
+            speed = 3
+        }
+
+        if(curr_video === -1){
+            if(videos.length > 0){
+                curr_video = 0
+                videos[curr_video].playbackRate = speed
+            }
+        } else {
+            videos[curr_video].playbackRate = speed
         }
     }
 
     if(request.type === "changeSpeed"){
         curr_speed = request.speed
-        
         change()
-
-        sendResponse({"done": true})
-        return true
     }
 
     if(request.type === "changeVideo"){
         curr_video = parseInt(request.video)
-
         change()
-
-        sendResponse({"done": true})
-        return true
     }
+
+    sendResponse()
+    return true
 })
--- src/popup.js
+++ src/popup.js
@@ -7,17 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
     function changeHandlerSpeed(event) {
         curr_speed = this.value
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {type: 'changeSpeed', "speed": curr_speed}, function(message){
-                console.log(message)
-            })
+            chrome.tabs.sendMessage(tabs[0].id, {type: 'changeSpeed', "speed": curr_speed}, function(resp){})
         })
     }
     function changeHandlerVideo(event) {
         curr_video = this.value
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {type: 'changeVideo', "video": curr_video}, function(message){
-                console.log(message)
-            })
+            chrome.tabs.sendMessage(tabs[0].id, {type: 'changeVideo', "video": curr_video}, function(resp){})
         })
     }
     Array.prototype.forEach.call(inputs, function(radio) {
@@ -27,38 +23,40 @@ document.addEventListener('DOMContentLoaded', function () {
     function getAllVideos(){
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {type: 'getAllVideos'}, function(message){
-                let tot_videos = message.tot_videos
+                if(message !== undefined && message.tot_videos !== undefined){
+                    let tot_videos = message.tot_videos
 
-                let div = document.createElement("div")
-                div.id = "videos"
-                div.style.marginTop = "10px"
-                let label = document.createElement("label")
-                if(tot_videos > 0){
-                    label.innerHTML = "video: "
-                } else {
-                    label.innerHTML = "No video found"
-                }
-                
-                div.append(label)
-                for(let a = 0; a < tot_videos; ++a){
-                    let inp = document.createElement("input")
-                    inp.type = "Radio"
-                    let name = (a+1).toString()
-                    inp.value = (a).toString()
-                    inp.name = "numVideo"
-
-                    if(a == 0){
-                        inp.checked = true
-                    }
-
-                    inp.addEventListener('change', changeHandlerVideo);
-
+                    let div = document.createElement("div")
+                    div.id = "videos"
+                    div.style.marginTop = "10px"
                     let label = document.createElement("label")
-                    label.innerHTML = name
+                    if(tot_videos > 0){
+                        label.innerHTML = "video: "
+                    } else {
+                        label.innerHTML = "No video found"
+                    }
+                    
+                    div.append(label)
+                    for(let a = 0; a < tot_videos; ++a){
+                        let inp = document.createElement("input")
+                        inp.type = "Radio"
+                        let name = (a+1).toString()
+                        inp.value = (a).toString()
+                        inp.name = "numVideo"
 
-                    div.append(inp, label)
+                        if(a == 0){
+                            inp.checked = true
+                        }
+
+                        inp.addEventListener('change', changeHandlerVideo);
+
+                        let label = document.createElement("label")
+                        label.innerHTML = name
+
+                        div.append(inp, label)
+                    }
+                    document.getElementById("root").append(div)
                 }
-                document.getElementById("root").append(div)
             })
         })
     }
